d2fe5138ca67e2873131843e54ee8d601f1bdca4
hasnain-a7 <husnainahmed519@gmail.com>
2025-09-01 14:05:28 +0500

    layout correct

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,5 +1,4 @@
-import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "../Components/Sidebar";
 import { auth } from "../Config/firbase";
 import { signOut } from "firebase/auth";
@@ -20,16 +19,17 @@ const Layout: React.FC = () => {
       console.error("Logout error:", error.message);
     }
   };
-  const handleSidbar = () => {
+
+  const handleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
-    <div className="relative flex flex-col h-dvh transition-all w-full">
+    <div className="relative flex flex-col h-dvh w-full transition-all">
       <nav className="w-full h-[60px] flex justify-between items-center bg-[#1a202c] text-white p-3">
         <div className="flex justify-between items-center w-full">
           <button
-            onClick={handleSidbar}
+            onClick={handleSidebar}
             className="p-2 hover:bg-gray-700 rounded-lg transition-colors duration-200 cursor-pointer"
           >
             <TfiAlignJustify className="w-6 h-6" />
@@ -40,18 +40,10 @@ const Layout: React.FC = () => {
       </nav>
 
       <div className="flex h-screen w-full overflow-hidden">
-        <div
-          className={`flex-shrink-0 transition-all duration-300 ease-in-out ${
-            isSidebarOpen ? "w-52" : "w-0"
-          }`}
-        >
-          {isSidebarOpen && (
-            <Sidebar isOpen={isSidebarOpen} toggleSidebar={handleSidbar} />
-          )}
-        </div>
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={handleSidebar} />
 
         <div className="flex flex-col flex-1 min-w-0 h-screen overflow-hidden">
-          <main className="flex-1 max-h-full  bg-gray-100 overflow-y-auto">
+          <main className="flex-1 max-h-full bg-gray-100 overflow-y-auto">
             <Outlet />
           </main>
         </div>
